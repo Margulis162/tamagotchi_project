8a1690caa552c6f14dc6a1eeb1a0ce7d6ce1b709
Margulis162 <markmargulis@proton.me>
2023-11-30 20:55:10 -0500

    nothing to watch here, move along

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 // var
 const http = require('http');
+const fs = require('fs');
 
 // f(x)
 // flow
 const server = http.createServer((req, res) => {
     console.log("request made");
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('hello');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
