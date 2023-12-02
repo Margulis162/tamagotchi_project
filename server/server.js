b39dc3ca7dcaccaa34ab2b2a0b417a77dc829222
Margulis162 <markmargulis@proton.me>
2023-12-01 21:15:04 -0500

    server response is a page now with boiler plate and style.css is attached to it

--- server/server.js
+++ server/server.js
@@ -8,8 +8,16 @@ const server = http.createServer((req, res) => {
     console.log("request made");
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('hello');
-    res.end();
+    
+    // send html
+    fs.readFile('../index.html', (err, data) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.write(data);
+            res.end(); 
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
